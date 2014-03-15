ad33643f5eeb983d6d3a9d333dc1d6813b443766
James Wang <jw@slickage.com>
2014-03-14 18:54:02 -1000

    Payment page finished without validation.
    * /pay?amount=1.23 shows a page with amount and new address obtained from
    bitcoind
    * /paymentqr shows a qr image with amount and address from querystring

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var config = require('./config');
+var path = require('path');
+var payments = require('./payments');
 var express = require('express');
+var qr = require('qr-image');
 var app = express();
+app.set('view engine', 'ejs');
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('/pay', function(req, res) {
+  var amount = req.query.amount;
+  payments.getPaymentAddress(function(err, address) {
+    res.render('pay', {
+      address: address,
+      amount: amount,
+      qrImageUrl: '/paymentqr?address=' + address + '&amount=' + amount
+    });
+  })
+});
+
+app.get('/paymentqr', function(req, res) {
+  var address = req.query.address;
+  if (!address) {
+    res.send('No address defined.');
+  }
+  var amount = req.query.amount || undefined;
+  var code = qr.image('bitcoin:' + address + '&amount=' + amount, { type: 'png' });
+  code.pipe(res);
 });
 
 app.listen(config.port);
