7310b72055297b5890a74b1d06165dd33e531d94
Anthony Kinsey <anthony@slickage.com>
2014-03-26 00:07:39 -1000

    Removing tab characters from validate.js

--- validate.js
+++ validate.js
@@ -1,23 +1,23 @@
 module.exports = {
-	invoice: function(invoice) {
-		if (!invoice.currency || !invoice.min_confirmations || !invoice.line_items ||
-				 invoice.line_items.length < 1 || !invoice.balance_due || 
-				 invoice.expiration < new Date().getTime()) {
-			 return null; 
-		}
-		else {
-			return invoice;
-		}
-	},
-	invoiceExpired: function(invoice) {
-		if (invoice.expiration) {
-			return invoice.expiration < new Date().getTime();
-		}
-		else {
-			return false;
-		}
-	},
-	objectID: function(str) {
+  invoice: function(invoice) {
+    if (!invoice.currency || !invoice.min_confirmations || !invoice.line_items ||
+         invoice.line_items.length < 1 || !invoice.balance_due || 
+         invoice.expiration < new Date().getTime()) {
+       return null; 
+    }
+    else {
+      return invoice;
+    }
+  },
+  invoiceExpired: function(invoice) {
+    if (invoice.expiration) {
+      return invoice.expiration < new Date().getTime();
+    }
+    else {
+      return false;
+    }
+  },
+  objectID: function(str) {
     str = str + '';
     var len = str.length, valid = false;
     if (len == 12 || len == 24) {
