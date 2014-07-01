9678c0bca60a8b5d6801d5965193c3cbdfb0b829
Warren Togami <wtogami@gmail.com>
2014-06-30 17:59:13 -1000

    Remove generatetoken.js

--- generatetoken.js
+++
@@ -1,7 +0,0 @@
-/* jshint node: true */
-'use strict';
-
-var crypto = require('crypto');
-var input = process.argv[2];
-var hash = crypto.createHash('sha256').update(input).digest('hex');
-console.log(hash);
