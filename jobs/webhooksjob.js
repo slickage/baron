627048186c09e5ce3f0079b930930abbaa7c66fa
Warren Togami <warren@slickage.com>
2014-07-20 15:18:21 -1000

    Some improvements to webhook retry logging
    * Print only _id's instead of entire webhook objects, it shouldn't be
      printing token secrets to the log.
    * Log entries should be a single line.
    * Change log level from info to debug.

--- jobs/webhooksjob.js
+++ jobs/webhooksjob.js
@@ -11,7 +11,13 @@ var async = require('async');
 function webhooksJob() {
   db.getWebhooks(function (err, webhooksArr) {
     if (!err && webhooksArr) {
-      log.info({ failedWebhooksArr: webhooksArr }, 'Retrying Failed Webhooks [' + webhooksArr.length + ']');
+      // Do not print secret tokens in log,
+      // also log entry should be a single line
+      var webhookIds = [];
+      async.eachSeries(webhooksArr, function(webhookObj, cb) {
+        webhookIds.push(webhookObj._id);
+      });
+      log.debug('Retrying Failed Webhooks [' + webhooksArr.length + ']' + JSON.stringify(webhookIds));
       async.eachSeries(webhooksArr, function(webhookObj, cb) {
         webhooks.postToWebhookIgnoreFailure(webhookObj, cb);
       },
